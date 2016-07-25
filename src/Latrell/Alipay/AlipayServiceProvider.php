ad16f2fdb4a71a46b56008cd536f450da45065d0
peiit <jiangpei@peiit.com>
2016-07-26 00:56:10 +0800

    format

--- src/Latrell/Alipay/AlipayServiceProvider.php
+++ src/Latrell/Alipay/AlipayServiceProvider.php
@@ -24,24 +24,23 @@ class AlipayServiceProvider extends ServiceProvider
 	protected function setupConfig()
 	{
 		$source_config = realpath(__DIR__ . '/../../config/config.php');
-	    $source_mobile = realpath(__DIR__ . '/../../config/mobile.php');
-	    $source_web = realpath(__DIR__ . '/../../config/web.php');
-	
-	    if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
-	        $this->publishes([
-	            $source_config => config_path('latrell-alipay.php'),
-	            $source_mobile => config_path('latrell-alipay-mobile.php'),
-	            $source_web => config_path('latrell-alipay-web.php'),
-	        ]);
-	    } elseif ($this->app instanceof LumenApplication) {
-	        $this->app->configure('latrell-alipay');
-	        $this->app->configure('latrell-alipay-mobile');
-	        $this->app->configure('latrell-alipay-web');
-	    }
-	
-	    $this->mergeConfigFrom($source_config, 'latrell-alipay');
-	    $this->mergeConfigFrom($source_mobile, 'latrell-alipay-mobile');
-	    $this->mergeConfigFrom($source_web, 'latrell-alipay-web');
+		$source_mobile = realpath(__DIR__ . '/../../config/mobile.php');
+		$source_web = realpath(__DIR__ . '/../../config/web.php');
+		if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+			$this->publishes([
+				$source_config => config_path('latrell-alipay.php'),
+				$source_mobile => config_path('latrell-alipay-mobile.php'),
+				$source_web => config_path('latrell-alipay-web.php'),
+			]);
+		} elseif ($this->app instanceof LumenApplication) {
+			$this->app->configure('latrell-alipay');
+			$this->app->configure('latrell-alipay-mobile');
+			$this->app->configure('latrell-alipay-web');
+		}
+		
+		$this->mergeConfigFrom($source_config, 'latrell-alipay');
+		$this->mergeConfigFrom($source_mobile, 'latrell-alipay-mobile');
+		$this->mergeConfigFrom($source_web, 'latrell-alipay-web');
 	}
 
 	/**
